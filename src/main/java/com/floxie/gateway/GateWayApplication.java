efbe1d987366c2e09da352e203f744a1befc0af9
Svetlin Dimitrov <svetlin.dimitrov@primeholding.com>
2025-03-03 09:42:50 +0200

    update gateway and dockerFile

--- src/main/java/com/floxie/gateway/GateWayApplication.java
+++ src/main/java/com/floxie/gateway/GateWayApplication.java
@@ -1,16 +1,14 @@
 package com.floxie.gateway;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class GateWayApplication {
   public static void main(String[] args) {
 
-    SpringApplication.run(GateWayApplication.class , args);
+    SpringApplication.run(GateWayApplication.class, args);
   }
 }
